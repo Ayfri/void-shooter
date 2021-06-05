91b7575c6f30db42a3043a47f90e3f74398315ea
Ayfri <pierre.ayfri@gmail.com>
2021-06-05 16:51:35 +0200

    style: Prettify code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,33 @@
-const gulp = require("gulp")
-const esbuild = require("gulp-esbuild")
-const exec = require("child_process").exec
+const gulp = require('gulp');
+const esbuild = require('gulp-esbuild');
+const exec = require('child_process').exec;
 
 function bundle() {
-  return gulp
-    .src("./src/index.ts")
-    .pipe(
-      esbuild({
-        outfile: "bundle.js",
-        sourcemap: "inline",
-        bundle: true,
-        target: ["chrome60", "firefox55", "safari11", "edge18"],
-        loader: {
-          ".ts": "ts",
-          ".json": "json",
-        },
-      })
-    )
-    .pipe(gulp.dest("./dist/"))
+	return gulp
+		.src('./src/index.ts')
+		.pipe(esbuild({
+			outfile: 'bundle.js',
+			sourcemap: 'inline',
+			bundle: true,
+			target: ['chrome60', 'firefox55', 'safari11', 'edge18'],
+			loader: {
+				'.ts': 'ts',
+				'.json': 'json',
+			},
+		}))
+		.pipe(gulp.dest('dist'));
 }
 
 function copyPublic() {
-  return gulp.src("public/**/*").pipe(gulp.dest("./dist"))
+	return gulp.src('public/**/*').pipe(gulp.dest('dist'));
 }
 
 function watch() {
-  exec("reload -b --dir=dist --port=5000", (err) => {
-    if (err) throw err
-  })
-  return gulp.watch("src/**/*.ts", gulp.series(bundle, copyPublic))
+	exec('reload -b --dir=dist --port=5000', (err) => {
+		if (err) throw err;
+	});
+	return gulp.watch('src/**/*.ts', gulp.series(bundle, copyPublic));
 }
 
-exports.bundle = gulp.series(bundle, copyPublic)
-exports.watch = gulp.series(bundle, copyPublic, watch)
+exports.bundle = gulp.series(bundle, copyPublic);
+exports.watch = gulp.series(bundle, copyPublic, watch);
